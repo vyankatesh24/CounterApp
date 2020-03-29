61c0e3a945d0c4bf1c432402d9cff9a5087857a5
Vyankatesh Berde <vyankateshberde24@gmail.com>
2020-03-29 12:05:03 +0530

    Added Option menu in Counter. Changed counter design

--- app/src/main/java/com/vyankatesh/counterapp/MainActivity.java
+++ app/src/main/java/com/vyankatesh/counterapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.vyankatesh.counterapp;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,46 +31,84 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        count_tv =  findViewById(R.id.count);
+        count_tv = findViewById(R.id.count);
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
         inputTarget = findViewById(R.id.input_target);
 
     }
 
-    public void reset(View view){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.counter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_reduce:
+                if (COUNT > 0)
+                    COUNT--;
+                count_tv.setText(COUNT + "");
+                Log.d(LOG_TAG, "Decremented to " + COUNT);
+                return true;
+
+            case R.id.menu_reset:
+                COUNT = 0;
+                Log.d(LOG_TAG, "Reset to " + COUNT);
+                count_tv.setText(COUNT + "");
+
+                return true;
+
+            case R.id.menu_set_target:
+                setTarget();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void reset(View view) {
         COUNT = 0;
         Log.d(LOG_TAG, "Reset to " + COUNT);
         count_tv.setText(COUNT + "");
     }
 
-    public void reduceCount(View view){
-        if (COUNT > 0)
-            COUNT--;
-        count_tv.setText(COUNT+"");
-        Log.d(LOG_TAG, "Decremented to " + COUNT);
+    public void increaseCount(View view) {
 
-    }
+        if (Target == 0)
+            setTarget();
 
-    public void increaseCount(View view){
-
-        if (Target != 0 && COUNT == Target - 1) {
-            COUNT++;
-            count_tv.setText(String.valueOf(COUNT));
-            Log.d(LOG_TAG, "Incremented to " + COUNT);
-            Toast.makeText(getApplicationContext(), "Target Achieved", Toast.LENGTH_LONG).show();
-        } else {
+        if (Target != 0 && COUNT < Target) {
             COUNT++;
             count_tv.setText(String.valueOf(COUNT));
             Log.d(LOG_TAG, "Incremented to " + COUNT);
         }
-
-
-
+        if (Target != 0 && COUNT == Target) {
+            Toast.makeText(getApplicationContext(), "Target Achieved", Toast.LENGTH_LONG).show();
+        }
     }
 
-    public void setTarget(View view) {
-        Target = Integer.parseInt(inputTarget.getText().toString());
-        Toast.makeText(getApplicationContext(), "Target has been set.", Toast.LENGTH_LONG).show();
+    public void setTarget() {
+
+        final View set_target_view = LayoutInflater.from(this).inflate(R.layout.set_target_layout, null);
+        final AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle("Set Target");
+        myAlertBuilder.setMessage("Enter your target count number");
+        myAlertBuilder.setView(set_target_view);
+        myAlertBuilder.setPositiveButton("Set Target", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText target = set_target_view.findViewById(R.id.set_target);
+                        Target = Integer.parseInt(target.getText().toString());
+                        Toast.makeText(getApplicationContext(), "Target has been set to" + Target, Toast.LENGTH_LONG).show();
+                    }
+                });
+        myAlertBuilder.show();
+
 
     }
 
